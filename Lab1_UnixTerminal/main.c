85239dfdbc52b01df7bb2a8e572ce8b3ccd048e4
Shashank Agrawal <sagrawal8@login-students.ccs.neu.edu>
2021-01-21 18:24:41 -0500

    Adding power function

--- Lab1_UnixTerminal/main.c
+++ Lab1_UnixTerminal/main.c
@@ -1,1 +1,26 @@
-// Add your program here!
+#include <stdio.h>
+#include <math.h>
+double power(double base, double n){
+	return  pow(base, n);
+}
+
+int main(){
+	int i;
+    char* str = "";
+	for(i = 0; i < 10; i++){
+		if(i == 0){
+			str = "st";
+        }
+        else if(i == 1){
+            str = "nd";
+        }
+        else if(i == 2){
+            str = "rd";
+        }
+        else{
+            str = "th";
+        }
+		printf("2 to the %d%s power is: %f\n", i+1, str, power(2, i));
+	}
+	
+}
