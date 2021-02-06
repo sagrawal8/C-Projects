2fd975bcec990da103bac12631711be473e2bdb6
Michael Shah <mikeshah@login-students.ccs.neu.edu>
2021-02-05 22:26:27 -0500

    adding: New Directory

---
+++ Assignment3_More_Data_Structures/part1/main.c
@@ -0,0 +1,28 @@
+// Compile this assignment with: gcc -g -Wall main.c -o main
+//
+// Include parts of the C Standard Library
+// These have been written by some other really
+// smart engineers.
+#include <stdio.h>  // For IO operations
+#include <stdlib.h> // for malloc/free
+
+// Our library that we have written.
+// Also, by a really smart engineer!
+#include "my_dll.h"
+// Note that we are locating this file
+// within the same directory, so we use quotations
+// and provide the path to this file which is within
+// our current directory.
+
+
+
+// ====================================================
+// ================== Program Entry ===================
+// ====================================================
+int main(){
+    dll_t * dll = create_dll();
+    
+    
+    
+    return 0;
+}
