5f0f85240603bb6b1f5c7cf01d32c844bc836100
sagrawal8 <61482309+sagrawal8@users.noreply.github.com>
2021-02-08 19:42:57 -0500

    Update barebones.s

--- Lab3_Assembly/part1/barebones.s
+++ Lab3_Assembly/part1/barebones.s
@@ -12,21 +12,24 @@ _barebones:
 main:
 					# (1) What are we setting up here?
 					# Ans:
-	pushq %rbp			#
-	movq  %rsp, %rbp		#
+	pushq %rbp			# Pushing quadword of %rbp which points to base of stack.
+	movq  %rsp, %rbp		# Moves a quadword of %rsp and %rbp, which is the top of stack and bottom of stack.
+					# Command< src , dest> top of stack is copied to bottom of stack? I'm guessing we are
+					# initializing a stack here?
 
 					# (2) What is going on here
 					# Ans:
-	movq $1, %rax			# 
-	movq $1, %rdi			#
-	leaq .hello.str,%rsi		#
+	movq $1, %rax			# stores value at direct memory address ($1) at %rax (accumulator register) (temp = $1)
+	movq $1, %rdi			# stores value at direct memory address ($1) at %rdi register. (We are defining capacity of stack? or first program argument (%rdi = $1))
+	leaq .hello.str,%rsi		# stores address of hello.str at register %rsi. ->loading address of string?
 
 
 					# (3) What is syscall? We did not talk about this
 					# in class.
-					# Ans:
+					# Ans:  A computer program makes a system call when it makes a request to the operating system’s kernel. 
+					# System call provides the services of the operating system to the user programs via Application Program Interface(API)
 	syscall				# Which syscall is being run?
-					# Ans:
+					# Ans: Pushing hello.str into stack?
 
 					# (4) What would another option be instead of 
 					# using a syscall to achieve this?
@@ -34,7 +37,7 @@ main:
 
 	movq	$60, %rax		# (5) We are again setting up another syscall
 	movq	$0, %rdi		# What command is it?
-					# Ans:	
+					# Ans:	Popping the stack?
 	syscall
 
 	popq %rbp			# (Note we do not really need
@@ -43,4 +46,4 @@ main:
 .hello.str:
 	.string "Hello World!\n"
 	.size	.hello.str,13		# (6) Why is there a 13 here?
-					# Ans:	
+					# Ans:	lenth of string is 13
