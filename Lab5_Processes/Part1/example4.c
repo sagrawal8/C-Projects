1055fa5b217da93e9c8e4b58518aed6d89fa8d8e
Shashank Agrawal <sagrawal8@login-students.ccs.neu.edu>
2021-02-21 13:47:01 -0500

    changing example4

--- Lab5_Processes/Part1/example4.c
+++ Lab5_Processes/Part1/example4.c
@@ -9,10 +9,14 @@ int main(){
     int child_status;
     if (fork() == 0){
         printf("Child runs!\n");
+        exit(0);
+    }
+    else{
         wait(&child_status);    
         if(WEXITSTATUS(child_status)>=0){
             printf("Child exited with the following child_status %d\n",WEXITSTATUS(child_status));
         }
+
         printf("Child always is terminated by this point--parent resuming\n");
     }
     printf("This always prints last\n");
