4502405dd7e4bc0fd712a3ff4700d0f3464ea652
Shashank Agrawal <sagrawal8@login-students.ccs.neu.edu>
2021-02-21 12:49:46 -0500

    Pushing all examples

--- Lab5_Processes/Part1/example4.c
+++ Lab5_Processes/Part1/example4.c
@@ -1,1 +1,20 @@
-// Type in and try example4.c
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/wait.h> // Library with the 'wait' system call.
+#include <unistd.h>
+int main(){
+
+    int child_status;
+    if (fork() == 0){
+        printf("Child runs!\n");
+        wait(&child_status);    
+        if(WEXITSTATUS(child_status)>=0){
+            printf("Child exited with the following child_status %d\n",WEXITSTATUS(child_status));
+        }
+        printf("Child always is terminated by this point--parent resuming\n");
+    }
+    printf("This always prints last\n");
+    return 0;
+}
--- Lab5_Processes/Part1/example5.c
+++ Lab5_Processes/Part1/example5.c
@@ -1,1 +1,24 @@
-// Type in and try example5.c
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <unistd.h>
+
+int main(){
+    char* myargv[16];
+    myargv[0] = "/bin/cat";
+    myargv[1] = "-n";
+    myargv[2]="example5.c"; 
+    myargv[3]=NULL;     
+    if(fork()==0){
+        execve(myargv[0],myargv,NULL);
+        printf("Child: Should never get here\n");
+        exit(1);
+    }
+    else{
+        wait(NULL);
+        printf("This always prints last\n");
+    }
+    
+    return 0;
+}
