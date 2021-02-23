1ffb6357d10a9d5c52d2fc07383270835ec83906
Shashank <github.com/sagrawal8>
2021-02-22 22:45:09 -0500

    Pushing working shell without pipe and running shell inside shell

--- Assignment5_Shell/part1/shell.c
+++ Assignment5_Shell/part1/shell.c
@@ -13,8 +13,9 @@
 void interactive_mode();
 void parse_arguments(char* line);
 void shell_error();
-int check_for_builtin(char* argc, char** args);
-int execute_shell_command(char* argc, char** args);
+int check_for_builtin(char** args);
+int execute_shell_command(char** args);
+void guessingGame();
 
 char error_message[30] = "An error has occurred\n";
 int arg_position = 0;
@@ -22,7 +23,6 @@ char *path;
 int arg_bufsize = BUFFER_SIZE;
 int path_bufsize = BUFFER_SIZE;
 char** argv;
-char* argc;
 int argCount = 0;
 pid_t pids[BUFFER_SIZE];
 int pid_position;
@@ -33,7 +33,7 @@ int main(){
 	builtin[0] = "exit";
 	builtin[1] = "cd";
 	builtin[2] = "help";
-	builtin[3] = "guessing game";
+	builtin[3] = "GuessingGame";
 	path = strdup("/bin/");
 	pid_position = 0;
 	interactive_mode();
@@ -42,7 +42,6 @@ int main(){
 
 void signal_handler(int sig){
 	write(1,"\nmini-shell terminated\n",23);
-	free(argc);
 	free(argv);
 	exit(0);
 }
@@ -56,7 +55,6 @@ void interactive_mode(){
 		char* line = NULL;
 		size_t bufsize = 0;
 		argv = malloc(arg_bufsize*sizeof(char*));
-		argc = malloc(arg_bufsize*sizeof(char*));
 		if(!argv) {
 			shell_error();
 		}
@@ -64,17 +62,18 @@ void interactive_mode(){
 			shell_error();
 		}
 		parse_arguments(line);
-		if(argc != NULL) {
-			if(check_for_builtin(argc, argv) != 1){
-				printf("builtin command not found,executing shell command\n");
-				execute_shell_command(argc, argv);
+		if(argv[0] != NULL) {
+			if(strcmp(argv[0], "") == 0 ){
+				continue;
+			}
+			if(check_for_builtin(argv) != 1){
+				printf("builtin command not found, executing shell command\n");
+				execute_shell_command(argv);
 			}
 			
 		}
-		free(argc);
 		free(argv);
 	}
-	free(argc);
 	free(argv);
 }
 
@@ -86,12 +85,9 @@ void parse_arguments(char* line){
 		return;
 	}
 	token = strtok(line, " \t\n");
-	argc = strdup(token);
-	printf("%s", argc);
-	token = strtok(NULL, " \t\n");
 	while(token != NULL) {
 		argv[arg_position] = strdup(token);
-		if(strcmp(arg_position, "|") == 0) {
+		if(strcmp(argv[arg_position], "|") == 0) {
 		pipeCheck = true;
 		}	
 		arg_position++;
@@ -111,47 +107,47 @@ void parse_arguments(char* line){
 
 void shell_error() {
 	write(STDERR_FILENO, error_message, strlen(error_message));
-	free(argc);
 	free(argv);
 }
 
 
-int check_for_builtin(char* argc, char** argv){
-	if(strcmp(argc, "exit") == 0) {
+int check_for_builtin(char** argv){
+	if(strcmp(argv[0], "exit") == 0) {
 		
-		if(arg_position != 0) {
+		if(arg_position != 1) {
 			shell_error();
 		}
 		printf("Mini-shell exiting\n");
-		free(argc);
 		free(argv);
 		exit(0);
 	
-	}else if(strcmp(argc, "cd") == 0) {
+	}else if(strcmp(argv[0], "cd") == 0) {
 		
-		if(arg_position != 1) {
+		if(arg_position != 2) {
 			printf("path not specified\n");
 		}
 		
 		char cwd[100];
         	if(getcwd(cwd, sizeof(cwd))!=NULL) {
-                	printf("current directory is %s\n", cwd);
+                	printf("Previous directory was %s\n", cwd);
         	}
 
 		
-		if(chdir(strdup(argv[0])) == -1){
+		if(chdir(strdup(argv[1])) == -1){
 			printf("changing directories failed\n");
+			printf("Directory is still %s\n", cwd);
+		
 		}
 		
         	if(getcwd(cwd, sizeof(cwd))!=NULL) {
-                	printf("current directory is %s\n", cwd);
+                	printf("Current directory is %s\n", cwd);
         	}	
 
 		return 1;
 		
-	}else if(strcmp(argc, "help") == 0) {
+	}else if(strcmp(argv[0], "help") == 0) {
 			
-		if(arg_position > 0) {
+		if(arg_position > 1) {
 			printf("Too many Arguments for help");
 		}
 		int i;
@@ -159,20 +155,22 @@ int check_for_builtin(char* argc, char** argv){
 			printf("%d. %s\n", i+1, builtin[i]);
 		}
 		return 1;	
-	}else if(strcmp(argc, "Guessing Game" == 0)) {
+	
+	}else if(strcmp(argv[0], "GuessingGame") == 0) {
+		
 		guessingGame();
-	else {
-		return 0;
+		return 1;
+	
 	}
 	return 0;
 
 }
 
-int execute_shell_command(char* argc, char** argv){
+int execute_shell_command(char** argv){
 	
 	char shell_command[BUFFER_SIZE];
 	strcpy(shell_command, path);
-	strcat(shell_command, argc);
+	strcat(shell_command, argv[0]);
 	printf("%s\n", shell_command);
 	if(pipeCheck == false){
        		if(fork() == 0){
@@ -183,6 +181,7 @@ int execute_shell_command(char* argc, char** argv){
 			wait(NULL);
 			printf("parent print\n");
 		}
+	}
 	else{
 		return 0;
 	}
@@ -191,7 +190,6 @@ int execute_shell_command(char* argc, char** argv){
 }
 
 void guessingGame(){
-	int guesses[5];
 	int counter = 0;
 	srand(time(NULL));
 	int correctAnswer = rand() % 10 + 1;
