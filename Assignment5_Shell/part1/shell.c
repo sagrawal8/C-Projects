5d6cc6792727319561ccdd1df5a690e912f35b91
Shashank <github.com/sagrawal8>
2021-02-26 11:45:49 -0500

    final shell working

--- Assignment5_Shell/part1/shell.c
+++ Assignment5_Shell/part1/shell.c
@@ -10,54 +10,63 @@
 #include <stdbool.h>
 #include <time.h>
 #define BUFFER_SIZE 80
-#define TIME_LIMIT 10 
+#define TIME_LIMIT 50 
+
 //Forward method declaration
 void interactive_mode();
 void parse_arguments(char* line);
 void shell_error();
 int check_for_builtin(char** args);
-int execute_shell_command(char** argv, char** argv_pipe);
+void execute_shell_command(char** argv, char** argv_pipe);
 void guessingGame();
 void free_arg_arrays();
 void alarm_handler(int);
 void child_handler(int);
-
+void fork_function(char** argv, int in, int out);
+void fork_error_handling(pid_t pid);
+//Globals for handlers
 int timeout = 0;
 int child_done = 0;
 
-char error_message[30] = "An error has occurred\n";
-int arg_position = 0;
-int pipe_position;
-char *path;
-int arg_bufsize = BUFFER_SIZE;
-int path_bufsize = BUFFER_SIZE;
-char** argv;
-char** argv_pipe;
-char* builtin[4];
-bool pipeCheck;
+//Global Variables
+char error_message[30] = "An error has occurred\n";	//error message for shell_error()
+int arg_position;                                    	//position + 1 of last element in argv array;
+int pipe_position;					//position + 1 of last element in argv_pipe array
+int arg_bufsize = BUFFER_SIZE;                          //Buffer for getline function in interactive_mode()
+char** argv;						//argv array
+char** argv_pipe;					//argv array when pipe present
+char* builtin[4];  					//built in array for help function
+bool pipeCheck;						//true when pipe present, false when not
+
+//Main function
+//Intialize a few global variables
+//Call method for infinite while loop
 int main(){
 	
 	builtin[0] = "exit";
 	builtin[1] = "cd";
 	builtin[2] = "help";
 	builtin[3] = "GuessingGame";
-	path = strdup("/bin/");
 	argv = NULL;
 	argv_pipe = NULL;
 	interactive_mode();
   	return 0;
 }
 
+//Signal Handler for ctrl + C
 void signal_handler(int sig){
 	write(1,"\nmini-shell terminated\n",23);
 	free_arg_arrays();
 	exit(0);
 }
 
+//Infinite while loop
+//checks for ctrl + c
+//Read user input and decide whether to execute shell or builtin function
+//free arrays after loop ends
 void interactive_mode(){
 
 	signal(SIGINT, signal_handler);
-	printf("mini-shell> ");
 	while(1)
 	{
 		pipeCheck = false;
@@ -68,61 +77,54 @@ void interactive_mode(){
 			printf("argv error\n");
 			shell_error();
 		}
-		
+		printf("mini-shell>> ");
+		//get line allocs a buffer if a buffer size of 0 is provided.
 		if(getline(&line, &bufsize, stdin) == -1) {
 			printf("getline error\n");
 			shell_error();
-			printf("mini-shell> ");
-			continue;
 		}
-		
+		//parsing arguments
 		parse_arguments(line);
-		if(pipeCheck == true && argv_pipe[0] == NULL) {
-			free_arg_arrays();
-			printf("mini-shell> ");
-			continue;
-		}
-		if(argv[0] != NULL) 
-		{	
-
-			//printf("argv[0] is %s\n", argv[0]);
-			//printf("pipe check is %d\n", pipeCheck);
-			//if(argv_pipe != NULL){
-			//	printf("argv_pipe[0] is %s\n",argv_pipe[0]);
-			//	printf("argv_pipe[1] is %s ",argv_pipe[1]);
-			//}
+		if(argv[0] != NULL) {
+			//continue if string is empty
 			if(strcmp(argv[0], "") == 0 ){
+				printf("\n");
 				continue;
 			}
+			
 			if(check_for_builtin(argv) != 1){
-				printf("Built-in command not found, executing shell command\n");
 				execute_shell_command(argv, argv_pipe);
 			}
-
-			printf("mini-shell> ");
-			free_arg_arrays();
-
 		}
-		
+		//free arrays and mallocd line
+		free_arg_arrays();
+		free(line);		
 	}
 }
 
+//Method to parse arguments
 void parse_arguments(char* line){
 	char *token;
 	pipe_position = 0;
 	arg_position = 0;
-	if(strcmp(line, "\n") == 0)
+	
+	//return if newline or emptystring character
+	if(strcmp(line, "\n") == 0 || strcmp(line, "") == 0)
 	{
 		return;
 	}
+	
 	token = strtok(line, " \t\n");
 	while(token != NULL) {
+		//check for pipe symbol
 		if(strcmp(token, "|") == 0) {
 			argv_pipe = malloc(arg_bufsize*sizeof(char*)); 
 			pipeCheck = true;
 			token = strtok(NULL, " \t\n");
 			continue;
 		}
+
+		//add to pipe array if pipe symbol found
 		if(pipeCheck == true)
 		{
 			argv_pipe[pipe_position] = strdup(token);
@@ -137,8 +139,8 @@ void parse_arguments(char* line){
                         	}	
 				continue;
                 	}
-
 		}
+		//add to argv array if pipe symbol not found or before its found
 		else
 		{	
 			argv[arg_position] = strdup(token);
@@ -153,34 +155,41 @@ void parse_arguments(char* line){
 		}
 		token = strtok(NULL, " \t\n");
 	}
+	//set position after last element in both arrays to NULL
 	argv[arg_position] = NULL;
 	if(pipeCheck == true){
 		argv_pipe[pipe_position] = NULL;
 	}
+	//free mallocd strdup token
 	free(token);
 
 }
 
+//shell error method
+//prints error message and frees arrays
 void shell_error() {
 	write(STDERR_FILENO, error_message, strlen(error_message));
 	free_arg_arrays();	
+	exit(1);
 }
 
+//Method to free argv and agrv_pipe array
 void free_arg_arrays() {
 	if(argv != NULL)
 	{
 		free(argv);
 		argv = NULL;
-		printf("Freed argv array\n");
 	}
 	if(argv_pipe != NULL)
 	{
 		free(argv_pipe);
 		argv_pipe = NULL;
-		printf("Freed pipe array\n");
 	}
 }
 
+//Checks for builtin functions
+//Checks if user input matches any builtin function
+//returns 1 if it matches, 0 if it doesnt.
 int check_for_builtin(char** argv){
 	if(strcmp(argv[0], "exit") == 0) {
 		
@@ -195,30 +204,22 @@ int check_for_builtin(char** argv){
 		
 		if(arg_position != 2) {
 			printf("path not specified\n");
+			return 1;
 		}
 		
-		char cwd[100];
-        	if(getcwd(cwd, sizeof(cwd))!=NULL) {
-                	printf("Previous directory was %s\n", cwd);
-        	}
-
-		
+		char cwd[500];
+        			
 		if(chdir(strdup(argv[1])) == -1){
 			printf("changing directories failed\n");
 			printf("Directory is still %s\n", cwd);
 		
 		}
-		
-        	if(getcwd(cwd, sizeof(cwd))!=NULL) {
-                	printf("Current directory is %s\n", cwd);
-        	}	
-
 		return 1;
 		
 	}else if(strcmp(argv[0], "help") == 0) {
 			
 		if(arg_position > 1) {
-			printf("Too many Arguments for help");
+			printf("Too many Arguments for help\n");
 		}
 		int i;
 		for(i = 0; i < 4; i++){
@@ -236,26 +237,15 @@ int check_for_builtin(char** argv){
 
 }
 
-int execute_shell_command(char** argv, char** argv_pipe){
+//Executes shell commands
+//If mini-shell is run from here, it will only run for 50 seconds
+//as defined by TIME_LIMIT below header file declarations.
+//Duration can be adjusted by changing the time there.
+void execute_shell_command(char** argv, char** argv_pipe){
 
-	char shell_command[BUFFER_SIZE];
-	if(strcmp(argv[0], "./shell") == 0)
-	{
-		strcpy(shell_command, argv[0]);
-	}
-	else {
-		strcpy(shell_command, path);
-		strcat(shell_command, argv[0]);
-	}
-	printf("Path: %s\n", shell_command);
-	printf("Argv[0]: %s\n", argv[0]);
-	printf("Argv[1]: %s\n", argv[1]);
-	if(argv_pipe != NULL) {
-		printf("Argv_Pipe[0]: %s\n", argv_pipe[0]);
-		printf("Argv_Pipe[1]: %s\n", argv_pipe[1]);
-	}
+	//If no pipe present
 	if(pipeCheck == false){
-		printf("PIPE CHECK FALSE");
+		
 		pid_t pid = fork();
 		if(pid == -1) 
 		{
@@ -263,147 +253,91 @@ int execute_shell_command(char** argv, char** argv_pipe){
 			exit(1);
 		}
        		if(pid == 0){
-			execve(shell_command, argv, NULL);
-			printf("Child will never go here\n");
+			execvp(argv[0], argv);
 			exit(1);
 		}else {
-			signal(SIGALRM, alarm_handler);
-			signal(SIGCHLD, child_handler);
-			alarm(TIME_LIMIT);
-			pause();
-
-			if(timeout) {
-				printf("alarm triggered\n");
-				int result = waitpid(pid, NULL, WNOHANG);
-				if(result == 0) {
-					printf("Killing Child\n");
-					kill(pid, 9);
-					wait(NULL);
-				}
-				else {
-					printf("alarm triggered but child finished normally\n");
-				}
-			}
-			else if (child_done) {
-				printf("child finished normally\n");
-				wait(NULL);
-			}
-			
+			fork_error_handling(pid);	
 		}
 	}
-	else if(pipeCheck == true) {
-		printf("Entered pipe function");
+	//if pipe present
+	else if(pipeCheck == true) 
+	{
 		pid_t pid;
-		pid_t pid2;
-		int in, fd[2];
-		in = 0;
+		int fd[2];
+		int child_status;
 		pipe(fd);
-		pid = fork();
-		if(pid == -1)
-		{
-			perror("Fork Failed");
-			exit(1);
+		//Left side of pipe
+		fork_function(argv, fd[1], fd[0]);
+		//Right side of pipe
+		if((pid = fork()) == 0)
+		{	
+			close(STDIN_FILENO);
+			dup2(fd[0], STDIN_FILENO);
+			close(fd[0]);
+			close(fd[1]);			
+			execvp(argv_pipe[0], argv_pipe);
+			exit(1);	
 		}
-
-		if(pid == 0)
-		{
-			if(in != 0)
-			{
-				dup2(in, 0);
-				close(in);
-			}
-			if(fd[1] != 1)
-			{
-				dup2(fd[1], 1);
-				close(fd[1]);
-			}
-			execvp(argv[0], argv);
-		}
-		else {
-			printf("Parent Print");
-			signal(SIGALRM, alarm_handler);
-			signal(SIGCHLD, child_handler);
-			alarm(TIME_LIMIT);
-			pause();
-
-			if(timeout) 
-			{
-				printf("Alarm triggered\n");
-				int result = waitpid(pid, NULL, WNOHANG);
-				if(result == 0)
-				{
-					printf("Killing child\n");
-					kill(pid, 9);
-					wait(NULL);
-				}
-				else
-				{
-					printf("alarm triggered but child finished normally\n");
-				}
-			}
-			else if(child_done)
-			{
-				printf("child finished normally\n");
-				wait(NULL);
-			}
+		else if(pid != 0) {
+			close(fd[0]);
 			close(fd[1]);
-			in = fd[0];
-			if(in != 0)
-			{
-				dup2 (in, 0);
-			}
-			pid2 = fork();
-			if(pid2 == -1)
-			{
-				perror("Fork Failed");
-				exit(1);
-			}
-			if(pid2 == 0){
-				execvp(argv_pipe[0], argv_pipe);
-				printf("Child should never go here\n");
-				exit(1);
-			}
-			else
-			{
-				signal(SIGALRM, alarm_handler);
-				signal(SIGCHLD, child_handler);
-				alarm(TIME_LIMIT);
-				pause();
-
-				if(timeout)
-				{
-					printf("alarm triggered\n");
-					int result = waitpid(pid, NULL, WNOHANG);
-					if(result == 0) {
-						printf("Killing Child\n");
-						kill(pid2, 9);
-						wait(NULL);
-					}
-					else {
-						printf("Alarm triggered but child finished normally\n");
-					}
-				}
-				else if(child_done){
-					printf("Child finished normally\n");
-					wait(NULL);
-				}
-				return 0;
-			}
+			fork_error_handling(pid);	
+			return;	
 		}
-
 	}
-	
-	return 0;
 }
 
+//Seperated fork function to help with error handling in case
+//fork runs too long.
+//For Left side of pipe
+void fork_function(char** argv, int in, int out) {
+	pid_t pid = fork();
+	if(pid == -1)
+	{
+        	perror("Fork Failed");
+               	exit(1);
+        }
+        if(pid == 0){
+        	close(STDOUT_FILENO);
+                dup2(in, STDOUT_FILENO);
+                close(in);
+                close(out);
+		execvp(argv[0], argv);
+        	exit(1);
+        }else {
+		fork_error_handling(pid);
+		return;		
+	}	
+}
+
+
+void fork_error_handling(pid_t pid) {
+	signal(SIGALRM, alarm_handler);
+        signal(SIGCHLD, child_handler);
+        alarm(TIME_LIMIT);
+        pause();
+        if(timeout) {
+	        int result = waitpid(pid, NULL, WNOHANG);
+                if(result == 0) {
+        	        kill(pid, 9);
+                        wait(NULL);
+                }
+        }
+        else if (child_done) {
+                wait(NULL);
+        }
+}
+
+//handle for SIGCHLD
 void child_handler(int sig) {
 	child_done = 1;
 }
-
+//handler for SIGALRM
 void alarm_handler(int sig){
 	timeout = 1;
 }
 
+//Guessing game where user guesses a number bw 1 and 10
 void guessingGame(){
 	int counter = 0;
 	srand(time(NULL));
@@ -425,4 +359,5 @@ void guessingGame(){
 			printf("Your guess is lower than correct answer\n");
 		}
 	}
+	return;
 }
