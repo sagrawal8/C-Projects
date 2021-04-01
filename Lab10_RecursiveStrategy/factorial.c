1606740a1cccc38cd727bb4afbdf87486e819d19
Michael Shah <mikeshah@login-students.ccs.neu.edu>
2021-04-01 11:14:16 -0400

    adding: New Directory

---
+++ Lab10_RecursiveStrategy/factorial.c
@@ -0,0 +1,26 @@
+// gcc factorial.c -o factorial
+// ./factorial
+
+#include <stdio.h>
+
+// TODO: Implement iterative solution here
+int factorial(int n){
+
+  return 0;
+};
+
+// TODO: Implement recursive solution here
+int factorial_rec(int n){
+
+  return 0;
+}
+
+int main(){
+
+  // Both of these should print the same result!
+  printf("factorial(10) = %d\n",factorial(10));
+  printf("factorial_rec(10) = %d\n",factorial_rec(10));
+
+
+  return 0;
+}
